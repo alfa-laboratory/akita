0553bc9cd7137d6b62ff09a24cbcbcf8600cadd3
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2017-03-01 17:12:12 +0300

    refactoring(*): inherit getting operation by literal logic to enum

--- src/main/java/ru/alfabank/tests/core/rest/OperationType.java
+++ src/main/java/ru/alfabank/tests/core/rest/OperationType.java
@@ -1,15 +1,32 @@
 package ru.alfabank.tests.core.rest;
 
+import lombok.NonNull;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 /**
  * Created by U_M0UKA on 26.01.2017.
  */
 public enum OperationType {
-    MORE,
-    LESS,
-    EQUAL,
-    MORE_O_EQUAL,
-    LESS_O_EQUAL,
-    END_WITH,
-    CONTAINS,
-    START_WITH
+    MORE(">"),
+    LESS("<"),
+    EQUAL("=="),
+    MORE_O_EQUAL(">="),
+    LESS_O_EQUAL("<=");
+//    END_WITH,
+//    CONTAINS,
+//    START_WITH
+
+    final String literal;
+    OperationType(@NonNull String literal) {
+        this.literal = literal;
+    }
+
+    public static OperationType getOperationByLiteral(@NonNull String literal) {
+        return Arrays.stream(OperationType.values())
+                .filter(literal::equals)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(literal));
+    }
 }
