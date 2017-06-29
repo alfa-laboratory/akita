0c0068886acdc6ca901baf09fa968027489ce815
U_M0VF6 <AYChernysheva@alfabank.ru>
2017-06-29 17:30:07 +0300

    added MobileChrome driver to run tests in mobile  and removed hardcode chrome from browser= in gradle.properties

---
+++ src/main/java/ru/alfabank/tests/core/drivers/MobileChrome.java
@@ -0,0 +1,28 @@
+package ru.alfabank.tests.core.drivers;
+
+import com.codeborne.selenide.WebDriverProvider;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MobileChrome implements WebDriverProvider {
+
+    @Override
+    public WebDriver createDriver(DesiredCapabilities capabilities) {
+        System.out.println("---------------run CustomMobileDriver---------------------");
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "Google Nexus 5");
+
+        Map<String, Object> chromeOptions = new HashMap<>();
+        chromeOptions.put("mobileEmulation", mobileEmulation);
+
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
+        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        desiredCapabilities.setBrowserName(desiredCapabilities.chrome().getBrowserName());
+        return new ChromeDriver(desiredCapabilities);
+    }
+}
