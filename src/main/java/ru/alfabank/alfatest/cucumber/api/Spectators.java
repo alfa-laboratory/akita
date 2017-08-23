4dd70a1915bc7713a5ff6293f1b91988b17353a2
MaDream <amkonstantinov@alfabank.ru>
2017-08-23 18:41:39 +0300

    javaDoc for Spectators initial

--- src/main/java/ru/alfabank/alfatest/cucumber/api/Spectators.java
+++ src/main/java/ru/alfabank/alfatest/cucumber/api/Spectators.java
@@ -6,19 +6,28 @@ import com.codeborne.selenide.SelenideElement;
 import java.util.Arrays;
 import java.util.Collection;
 
-/**
- * Created by ruslanmikhalev on 26/01/17.
- */
 public final class Spectators {
 
     private Spectators() {
 
     }
 
+    /**
+     * Обертка над Selenide.waitUntil для произвольного числа элементов
+     *
+     * @param ec Selenide.Condition
+     * @param timeout максимальное время ожидания для перехода элементов в заданное состояние
+     * @param els произвольное количество selenide-элементов
+     */
     public static void waitElementsUntil(Condition ec, int timeout, SelenideElement... els) {
         Arrays.stream(els).forEach(e -> e.waitUntil(ec, timeout));
     }
 
+    /**
+     * Обертка над Selenide.waitUntil для работы с колекцией элементов
+     *
+     * @param els коллекция selenide-элементов
+     */
     public static void waitElementsUntil(Condition ec, int timeout, Collection<SelenideElement> els) {
         els.forEach(e -> e.waitUntil(ec, timeout));
     }
