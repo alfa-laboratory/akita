ad86504c3dd19956396b5875b6f51e1a5d93c22a
MaDream <amkonstantinov@alfabank.ru>
2017-09-01 16:34:01 +0300

    unexpectedly skipped merge conflict resolved

--- src/main/java/ru/alfabank/alfatest/cucumber/utils/Reflection.java
+++ src/main/java/ru/alfabank/alfatest/cucumber/utils/Reflection.java
@@ -13,16 +13,11 @@ public final class Reflection {
 
     }
 
-<<<<<<< HEAD
-    public static Object extractFieldValue(Field field, Object owner) {
-        field.setAccessible(true);
-=======
     /**
      * Получение поля класса с помощью механизма рефлексии
      */
-    public static Object extractFieldValue(Field f, Object owner) {
-        f.setAccessible(true);
->>>>>>> e356702bb47ab4a4f1aebacda8a1bb92f5fc9531
+    public static Object extractFieldValue(Field field, Object owner) {
+            field.setAccessible(true);
         try {
             return field.get(owner);
         } catch (IllegalAccessException e) {
