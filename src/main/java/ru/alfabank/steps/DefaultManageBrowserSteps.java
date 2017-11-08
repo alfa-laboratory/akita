3044617d4693bb35c35309b4158cf3e349f4edfb
iamstepanova <amstepanova@alfabank.ru>
2017-11-08 18:05:45 +0300

    Добавлен класс DefaultManageBrowserSteps для работы с куками. Добавлены шаги: @Когда("^куки приложения очищены$") @Когда("^кука из property файла с именем \"([^\"]*)\" сохранена в переменную \"([^\"]*)\"$") @Когда("^куки сохранены в переменную \"([^\"]*)\"$") @Когда("^добавлена кука с параметрами domain \"([^\"]*)\" name \"([^\"]*)\" и value \"([^\"]*)\" из property файла$")

---
+++ src/main/java/ru/alfabank/steps/DefaultManageBrowserSteps.java
@@ -0,0 +1,54 @@
+package ru.alfabank.steps;
+
+import cucumber.api.java.ru.Когда;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.Cookie;
+import ru.alfabank.alfatest.cucumber.api.AkitaScenario;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static ru.alfabank.tests.core.helpers.PropertyLoader.loadProperty;
+
+/**
+ * Шаги для работы с куками
+ */
+@Slf4j
+public class DefaultManageBrowserSteps {
+
+    private AkitaScenario akitascenario = AkitaScenario.getInstance();
+
+    /**
+     * Удаляем все куки
+     */
+    @Когда("^куки приложения очищены$")
+    public void deleteCookies(){
+        getWebDriver().manage().deleteAllCookies();
+    }
+
+    /**
+     * Ищем куку по имени (из property-файла). Сохраняем куку в переменную для дальнейшего использования
+     */
+    @Когда("^кука из property файла с именем \"([^\"]*)\" сохранена в переменную \"([^\"]*)\"$")
+    public void saveCookieToVar(String nameCookie, String cookie){
+        String cookieName = loadProperty(nameCookie);
+        String var = getWebDriver().manage().getCookieNamed(cookieName).toString();
+        akitascenario.setVar(cookie, var);
+    }
+
+    /**
+     * Сохраняем все куки в переменную для дальнейшего использования
+     */
+    @Когда("^куки сохранены в переменную \"([^\"]*)\"$")
+    public void saveAllCookies(String cookies){
+        String var = getWebDriver().manage().getCookies().toString();
+        akitascenario.setVar(cookies, var);
+    }
+
+    /**
+     * Собираем куку из параметров из property-файла и добавляем ее
+     */
+    @Когда("^добавлена кука с параметрами domain \"([^\"]*)\" name \"([^\"]*)\" и value \"([^\"]*)\" из property файла$")
+    public void addCookie( String cookieDomain, String cookieName, String cookieValue){
+        Cookie cookie = new Cookie(cookieDomain, cookieName, cookieValue);
+        getWebDriver().manage().addCookie(cookie);
+    }
+}
