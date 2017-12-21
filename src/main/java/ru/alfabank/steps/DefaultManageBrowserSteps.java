41f5517770067564f666c9c223fc4bb2e789d4a8
iamstepanova <amstepanova@alfabank.ru>
2017-12-21 11:22:32 +0300

    Исправлены замечания

--- src/main/java/ru/alfabank/steps/DefaultManageBrowserSteps.java
+++ src/main/java/ru/alfabank/steps/DefaultManageBrowserSteps.java
@@ -5,11 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.Cookie;
 import ru.alfabank.alfatest.cucumber.api.AkitaScenario;
 
+import java.util.Set;
+
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static ru.alfabank.tests.core.helpers.PropertyLoader.loadProperty;
+import static ru.alfabank.steps.DefaultApiSteps.resolveVars;
 
 /**
- * Шаги для работы с куками
+ * Шаги для работы с cookies
  */
 @Slf4j
 public class DefaultManageBrowserSteps {
@@ -17,48 +19,39 @@ public class DefaultManageBrowserSteps {
     private AkitaScenario akitascenario = AkitaScenario.getInstance();
 
     /**
-     * Удаляем все куки
+     * Удаляем все cookies
      */
-    @Когда("^куки приложения очищены$")
+    @Когда("^cookies приложения очищены$")
     public void deleteCookies(){
         getWebDriver().manage().deleteAllCookies();
     }
 
     /**
-     * Ищем куку по имени (из property-файла). Сохраняем куку в переменную для дальнейшего использования
+     * Ищем cookie по имени. Сохраняем cookie в переменную для дальнейшего использования
      */
-    @Когда("^кука из property файла с именем \"([^\"]*)\" сохранена в переменную \"([^\"]*)\"$")
+    @Когда("^cookie с именем \"([^\"]*)\" сохранена в переменную \"([^\"]*)\"$")
     public void saveCookieToVar(String nameCookie, String cookie){
-        String cookieName = loadProperty(nameCookie);
-        String var = getWebDriver().manage().getCookieNamed(cookieName).toString();
+        String cookieName = resolveVars(nameCookie);
+        Cookie var = getWebDriver().manage().getCookieNamed(cookieName);
         akitascenario.setVar(cookie, var);
     }
 
     /**
-     * Сохраняем все куки в переменную для дальнейшего использования
+     * Сохраняем все cookies в переменную для дальнейшего использования
      */
-    @Когда("^куки сохранены в переменную \"([^\"]*)\"$")
-    public void saveAllCookies(String cookies){
-        String var = getWebDriver().manage().getCookies().toString();
-        akitascenario.setVar(cookies, var);
+    @Когда("^cookies сохранены в переменную \"([^\"]*)\"$")
+    public void saveAllCookies(String variableName){
+        Set cookies = getWebDriver().manage().getCookies();
+        akitascenario.setVar(variableName, cookies);
     }
 
     /**
-     * Собираем куку из параметров из property-файла и добавляем ее
+     * Находим cookie по имени и подменяем ее значение. Имя cookie и домен не меняются
      */
-    @Когда("^добавлена кука с параметрами domain \"([^\"]*)\" name \"([^\"]*)\" и value \"([^\"]*)\" из property файла$")
-    public void addCookie( String cookieDomain, String cookieName, String cookieValue){
-        Cookie cookie = new Cookie(cookieDomain, cookieName, cookieValue);
-        getWebDriver().manage().addCookie(cookie);
-    }
-
-    /**
-     * Находим куку по имени и подменяем ее значение. Имя куки и домен не меняются
-     */
-    @Когда("^для куки с именем \"([^\"]*)\" подменяем значение на значение \"([^\"]*)\" из property файла$")
+    @Когда("^добавлена cookie с именем \"([^\"]*)\" и значением \"([^\"]*)\"$")
     public void replaceCookie(String cookieName, String cookieValue){
-        String nameCookie = loadProperty(cookieName);
-        String valueCookie = loadProperty(cookieValue);
+        String nameCookie = resolveVars(cookieName);
+        String valueCookie = resolveVars(cookieValue);
         getWebDriver().manage().addCookie(new Cookie(nameCookie, valueCookie));
     }
 }
