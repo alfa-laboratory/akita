56e2da235e6f9538091c9d51d203eac44c9ed7cb
mutaliska <alisa0301@mail.ru>
2018-02-08 14:42:17 +0300

    Refactor ApiSteps to send requests properly in simpler way #31
    * return current scenario
    change some methods on selenide
    remove constructor from DefaultManageBrowserSteps
    fix some tests
    
    * refactor DefaultApiSteps to send requests properly in simpler way #31

--- src/main/java/ru/alfabank/steps/DefaultApiSteps.java
+++ src/main/java/ru/alfabank/steps/DefaultApiSteps.java
@@ -19,6 +19,7 @@ import cucumber.api.java.ru.И;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSender;
+import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import ru.alfabank.alfatest.cucumber.api.AkitaScenario;
 import ru.alfabank.tests.core.rest.RequestParam;
@@ -28,9 +29,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -50,48 +49,39 @@ public class DefaultApiSteps {
     private AkitaScenario akitaScenario = AkitaScenario.getInstance();
 
     /**
-     * Посылается http GET/POST запрос по заданному урлу без параметров и BODY.
+     * Посылается http запрос по заданному урлу без параметров и BODY.
      * Результат сохраняется в заданную переменную
      * URL можно задать как напрямую в шаге, так и указав в application.properties
      */
-    @И("^выполнен (GET|POST) запрос на URL \"([^\"]*)\". Полученный ответ сохранен в переменную \"([^\"]*)\"$")
-    public void sendHttpRequestWithoutParams(String typeOfRequest, String address, String variableName) throws Exception {
-        String valueIfNotFoundInProperties = resolveVars(address);
-        address = loadProperty(address, valueIfNotFoundInProperties);
-        sendHttpRequest(typeOfRequest, address, variableName, new ArrayList<>());
+    @И("^выполнен (GET|POST|PUT|DELETE) запрос на URL \"([^\"]*)\". Полученный ответ сохранен в переменную \"([^\"]*)\"$")
+    public void sendHttpRequestWithoutParams(String method, String address, String variableName) throws Exception {
+        Response response = sendRequest(method, address, new ArrayList<>());
+        getBodyAndSaveToVariable(variableName, response);
     }
 
     /**
-     * Посылается http GET/POST запрос по заданному урлу с заданными параметрами.
+     * Посылается http запрос по заданному урлу с заданными параметрами.
      * И в URL, и в значениях в таблице можно использовать переменные и из application.properties, и из хранилища переменных
      * из AlfaScenario. Для этого достаточно заключить переменные в фигурные скобки, например: http://{hostname}?user={username}.
      * Content-Type при необходимости должен быть указан в качестве header.
      * Результат сохраняется в заданную переменную
      */
-    @И("^выполнен (GET|POST) запрос на URL \"([^\"]*)\" с headers и parameters из таблицы. Полученный ответ сохранен в переменную \"([^\"]*)\"$")
-    public void sendHttpRequestSaveResponse(String typeOfRequest, String address, String variableName, List<RequestParam> paramsTable) throws Exception {
-        String valueIfNotFoundInProperties = resolveVars(address);
-        address = loadProperty(address, valueIfNotFoundInProperties);
-        RequestSender request = createRequestByParamsTable(paramsTable);
-        Response response = request.request(Method.valueOf(typeOfRequest), address);
-        getResponseAndSaveToVariable(variableName, response);
+    @И("^выполнен (GET|POST|PUT|DELETE) запрос на URL \"([^\"]*)\" с headers и parameters из таблицы. Полученный ответ сохранен в переменную \"([^\"]*)\"$")
+    public void sendHttpRequestSaveResponse(String method, String address, String variableName, List<RequestParam> paramsTable) throws Exception {
+        Response response = sendRequest(method, address, paramsTable);
+        getBodyAndSaveToVariable(variableName, response);
     }
 
     /**
-     * Посылается http GET/POST запрос по заданному урлу с заданными параметрами.
+     * Посылается http запрос по заданному урлу с заданными параметрами.
      * Проверяется, что код ответа соответствует ожиданиям.
      * URL можно задать как напрямую в шаге, так и указав в application.properties
      * Content-Type при необходимости должен быть указан в качестве header.
      */
-    @И("^выполнен (GET|POST) запрос на URL \"([^\"]*)\" с headers и parameters из таблицы. Ожидается код ответа: (\\d+)$")
-    public void checkResponseCode(String typeOfRequest, String address, int expectedStatusCode, List<RequestParam> paramsTable) throws Exception {
-        String valueIfNotFoundInProperties = resolveVars(address);
-        address = loadProperty(address, valueIfNotFoundInProperties);
-        assertTrue(checkStatusCode(typeOfRequest, address, expectedStatusCode, paramsTable));
-    }
-
-    private RequestSender createRequestByParamsTable() {
-        return given().when();
+    @И("^выполнен (GET|POST|PUT|DELETE) запрос на URL \"([^\"]*)\" с headers и parameters из таблицы. Ожидается код ответа: (\\d+)$")
+    public void checkResponseCode(String method, String address, int expectedStatusCode, List<RequestParam> paramsTable) throws Exception {
+        Response response = sendRequest(method, address, paramsTable);
+        assertTrue(checkStatusCode(response, expectedStatusCode));
     }
 
     /**
@@ -100,57 +90,46 @@ public class DefaultApiSteps {
      * @param paramsTable массив с параметрами
      * @return сформированный запрос
      */
-    private RequestSender createRequestByParamsTable(List<RequestParam> paramsTable) {
-        Map<String, String> headers = new HashMap<>();
-        Map<String, String> parameters = new HashMap<>();
+    private RequestSender createRequest(List<RequestParam> paramsTable) {
         String body = null;
+        RequestSpecification request = given();
         for (RequestParam requestParam : paramsTable) {
-            String paramValue = resolveVars(requestParam.getValue());
-            String paramName = requestParam.getName();
+            String value = resolveVars(requestParam.getValue());
+            String name = requestParam.getName();
             switch (requestParam.getType()) {
                 case PARAMETER:
-                    parameters.put(paramName, paramValue);
+                    request.param(name, value);
                     break;
                 case HEADER:
-                    headers.put(paramName, paramValue);
+                    request.header(name, value);
                     break;
                 case BODY:
-                    String folderNameForRequestBodies = getPropertyOrValue("requestBodies");
-                    Path path = Paths.get("src", "main", "java", folderNameForRequestBodies, paramValue);
+                    String folder = getPropertyOrValue("requestBodies");
+                    Path path = Paths.get("src", "main", "java", folder, value);
                     try {
                         body = new String(Files.readAllBytes(path), "UTF-8");
                     } catch (IOException e) {
-                        body = paramValue;
+                        body = value;
                     }
+                    request.body(body);
                     break;
                 default:
-                    throw new IllegalArgumentException(String.format("Некорректно задан тип %s для параметра запроса %s ", requestParam.getType(), paramName));
+                    throw new IllegalArgumentException(String.format("Некорректно задан тип %s для параметра запроса %s ", requestParam.getType(), name));
             }
         }
-        RequestSender request;
         if (body != null) {
             akitaScenario.write("Тело запроса:\n" + body);
-            request = given()
-                    .headers(headers)
-                    .params(parameters)
-                    .body(body)
-                    .when();
-        } else {
-            request = given()
-                    .headers(headers)
-                    .params(parameters)
-                    .when();
         }
         return request;
     }
 
     /**
-     * Получает ответ на http запрос и сохраняет в переменную
+     * Получает body из ответа и сохраняет в переменную
      *
      * @param variableName имя переменной, в которую будет сохранен ответ
      * @param response     ответ от http запроса
      */
-    private void getResponseAndSaveToVariable(String variableName, Response response) {
+    private void getBodyAndSaveToVariable(String variableName, Response response) {
         if (response.statusCode() >= 200 && response.statusCode() < 300) {
             akitaScenario.setVar(variableName, response.getBody().asString());
             if (log.isDebugEnabled()) akitaScenario.write("Тело ответа : \n" + response.getBody().asString());
@@ -187,19 +166,13 @@ public class DefaultApiSteps {
     }
 
     /**
-     * Отправка запроса и сравнение кода http ответа
+     * Сравнение кода http ответа с ожидаемым
      *
-     * @param typeOfRequest      тип http запроса
-     * @param address            url, на который будет отправлен запрос
+     * @param response           ответ от сервиса
      * @param expectedStatusCode ожидаемый http статус код
-     * @param paramsTable        список параметров для http запроса
      * @return возвращает true или false в зависимости от ожидаемого и полученного http кодов
-     * @throws Exception
      */
-    public boolean checkStatusCode(String typeOfRequest, String address, int expectedStatusCode, List<RequestParam> paramsTable) throws Exception {
-        address = resolveVars(address);
-        RequestSender request = createRequestByParamsTable(paramsTable);
-        Response response = request.request(Method.valueOf(typeOfRequest), address);
+    public boolean checkStatusCode(Response response, int expectedStatusCode) {
         int statusCode = response.getStatusCode();
         if (statusCode != expectedStatusCode) {
             akitaScenario.write("Получен неверный статус код ответа " + statusCode + ". Ожидаемый статус код " + expectedStatusCode);
@@ -207,20 +180,17 @@ public class DefaultApiSteps {
         return statusCode == expectedStatusCode;
     }
 
-
     /**
-     * @param typeOfRequest тип http запроса
-     * @param address       url, на который будет направлен запрос
-     * @param variableName  имя переменной, в которую сохраняется http ответ
+     * Отправка http запроса
+     *
+     * @param method        тип http запроса
+     * @param address       url, на который будет направлен запроc
      * @param paramsTable   список параметров для http запроса
-     * @throws Exception
      */
-    private void sendHttpRequest(String typeOfRequest, String address, String
-            variableName, List<RequestParam> paramsTable) throws Exception {
-        String valueIfNotFoundInProperties = resolveVars(address);
-        address = loadProperty(address, valueIfNotFoundInProperties);
-        RequestSender request = createRequestByParamsTable(paramsTable);
-        Response response = request.request(Method.valueOf(typeOfRequest), address);
-        getResponseAndSaveToVariable(variableName, response);
+    private Response sendRequest(String method, String address,
+                                     List<RequestParam> paramsTable) {
+        address = loadProperty(address, resolveVars(address));
+        RequestSender request = createRequest(paramsTable);
+        return request.request(Method.valueOf(method), address);
     }
 }
