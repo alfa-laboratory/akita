7fb9491dd8af4e18a4477fa10403472859dd11ae
Alisaksandr <aleksandr_zdrachek@protonmail.com>
2018-08-29 22:32:56 +0300

    1. Чтение проперти headless из системных и апп пропертей 2. Воркэраунд для установки версии фаерфокса 3. Тест на запуск фаерфокса

--- src/test/java/ru/alfabank/core/CustomDriverProviderTests.java
+++ src/test/java/ru/alfabank/core/CustomDriverProviderTests.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2017 Alfa Laboratory
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,10 +27,21 @@ public class CustomDriverProviderTests {
 
     @Test
     public void createChromeDriverTest() {
+        System.setProperty("browser", "chrome");
         CustomDriverProvider customDriverProvider = new CustomDriverProvider();
         WebDriver currentDriver;
         currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
         assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.chrome.ChromeDriver"));
+        currentDriver.quit();
+    }
+
+    @Test
+    public void createFirefoxDriverTest() {
+        System.setProperty("browser", "firefox");
+        CustomDriverProvider customDriverProvider = new CustomDriverProvider();
+        WebDriver currentDriver;
+        currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
+        assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.firefox.FirefoxDriver"));
         currentDriver.quit();
     }
 }
