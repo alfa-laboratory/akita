81c82c36be084ccafa43c3a897b173d79408915c
Stanislav Frolov <sfrolov2@alfabank.ru>
2018-12-12 20:19:50 +0300

    resolve merge conflict

--- src/test/java/ru/alfabank/core/CustomDriverProviderTests.java
+++ src/test/java/ru/alfabank/core/CustomDriverProviderTests.java
@@ -29,14 +29,10 @@ public class CustomDriverProviderTests {
     @Test
     public void createChromeDriverTest() {
         System.setProperty("browser", "chrome");
-        System.setProperty("width", "666");
-        System.setProperty("height", "666");
         CustomDriverProvider customDriverProvider = new CustomDriverProvider();
         WebDriver currentDriver;
         currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
         assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.chrome.ChromeDriver"));
-        assertThat(currentDriver.manage().window().getSize().getWidth(), is(666));
-        assertThat(currentDriver.manage().window().getSize().getHeight(), is(666));
         currentDriver.quit();
     }
 
@@ -44,14 +40,10 @@ public class CustomDriverProviderTests {
     @Test
     public void createFirefoxDriverTest() {
         System.setProperty("browser", "firefox");
-        System.setProperty("width", "1000");
-        System.setProperty("height", "1000");
         CustomDriverProvider customDriverProvider = new CustomDriverProvider();
         WebDriver currentDriver;
         currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
         assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.firefox.FirefoxDriver"));
-        assertThat(currentDriver.manage().window().getSize().getWidth(), is(1000));
-        assertThat(currentDriver.manage().window().getSize().getHeight(), is(1000));
         currentDriver.quit();
     }
 
@@ -59,14 +51,10 @@ public class CustomDriverProviderTests {
     @Test
     public void createOperaDriverTest() {
         System.setProperty("browser", "opera");
-        System.setProperty("width", "500");
-        System.setProperty("height", "500");
         CustomDriverProvider customDriverProvider = new CustomDriverProvider();
         WebDriver currentDriver;
         currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
         assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.opera.OperaDriver"));
-        assertThat(currentDriver.manage().window().getSize().getWidth(), is(500));
-        assertThat(currentDriver.manage().window().getSize().getHeight(), is(500));
         currentDriver.quit();
     }
 
@@ -103,25 +91,12 @@ public class CustomDriverProviderTests {
     }
 
     @Test
-    public void createNonexistentDriverTest() {
+    public void createDefaultDriverTest() {
         System.setProperty("browser", "nonexistent");
         CustomDriverProvider customDriverProvider = new CustomDriverProvider();
         WebDriver currentDriver;
         currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
         assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.chrome.ChromeDriver"));
-        assertThat(currentDriver.manage().window().getSize().getWidth(), is(1920));
-        assertThat(currentDriver.manage().window().getSize().getHeight(), is(1080));
-        currentDriver.quit();
-    }
-
-    @Test
-    public void createEmptyDriverTest() {
-        CustomDriverProvider customDriverProvider = new CustomDriverProvider();
-        WebDriver currentDriver;
-        currentDriver = customDriverProvider.createDriver(new DesiredCapabilities());
-        assertThat(currentDriver.getClass().getName(), is("org.openqa.selenium.chrome.ChromeDriver"));
-        assertThat(currentDriver.manage().window().getSize().getWidth(), is(1920));
-        assertThat(currentDriver.manage().window().getSize().getHeight(), is(1080));
         currentDriver.quit();
     }
 }
