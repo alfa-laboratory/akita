9e0e3a844ab3595e0ceb3a575cb68928e3752819
daniilmoiseev <damoiseev@alfabank.ru>
2021-06-24 14:02:35 +0300

    #AUTOTESTS-2195 add function read properties from file with filename

--- src/main/java/ru/alfabank/tests/core/helpers/PropertyLoader.java
+++ src/main/java/ru/alfabank/tests/core/helpers/PropertyLoader.java
@@ -1,4 +1,4 @@
-***
+/**
         * Copyright 2017 Alfa Laboratory
         * Licensed under the Apache License, Version 2.0 (the "License");
         * you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
         * See the License for the specific language governing permissions and
         * limitations under the License.
         */
-        package ru.alfabank.tests.core.helpers;
+package ru.alfabank.tests.core.helpers;
 
 import com.google.common.base.Strings;
 import lombok.SneakyThrows;
